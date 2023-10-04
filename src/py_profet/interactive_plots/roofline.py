bdf8edbb1f1381e6d3014736b9bbfc4a264ac9ac
vxirau <victor.xirau@salle.url.edu>
2023-10-04 10:37:40 +0200

    roofline optimized

--- src/py_profet/interactive_plots/roofline.py
+++ src/py_profet/interactive_plots/roofline.py
@@ -1,3 +1,4 @@
+import math
 import numpy as np
 import plotly.graph_objects as go
 import plotly.express as px
@@ -89,14 +90,20 @@ def softplus(x):
     return np.log(1 + np.exp(x))
 
 def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_transparency, labels, stress_score_scale, graph_title=''):
-    
-    operational_intensity = np.linspace(0, 100000, 100000)
-    mem_perf = operational_intensity * peak_bw_gbs
-    mem_bound_performance = np.minimum(mem_perf, peak_flopss)
-    
+
     num_rows = len(df)
     df['flops/s'] = np.random.uniform(0, peak_flopss, num_rows)
     df['flops/byte'] = df['flops/s'] / (df['bw'] * 40) #TODO: This *40 multiplier is arbitrary, but allowed for data to be positioned to the left of the graph. This should be revised.
+
+    filtered_values = df['flops/byte'][np.isfinite(df['flops/byte'])]
+
+    # Check if there are any non-infinite values left
+    if len(filtered_values) > 0:
+        max_x_value = round(filtered_values.max())+10000
+    else:
+        max_x_value = 10000
+
+    fig = go.Figure()
 
     '''
     #Only compute roof stress score
@@ -119,6 +126,9 @@ def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_tran
     normalized_distance_compute = distance_compute / peak_flopss
     stress_score_compute = np.clip(1 - normalized_distance_compute, 0, 1)
 
+    operational_intensity = np.linspace(0, max_x_value, max_x_value)
+    mem_perf = operational_intensity * peak_bw_gbs
+    mem_bound_performance = np.minimum(mem_perf, peak_flopss)
     proximity_memory = 1 - (df['flops/byte'] / mem_bound_performance[df.index])
 
     weight_compute = 0.7 
@@ -126,63 +136,36 @@ def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_tran
 
     df['stress_score'] = (weight_compute * stress_score_compute + weight_memory * proximity_memory)
 
-    decay_factor = 1.2
+    decay_factor = 1
     df['stress_score'] = 1 - np.exp(-decay_factor * df['stress_score'])
-  
+
     x_data = np.array(df['flops/byte'])
     y_data = np.array(df['flops/s'])
-    
-    matching_dict = next((item for item in cache_bw if item['value'] == peak_bw_gbs), None)
 
-    cache_bound_performance = []
-        #cache_bound_performance = np.minimum(np.outer(operational_intensity, cache_bw), peak_flopss)
-
-    for i in range(len(cache_bw)):
-        cache_bound_performance.append(operational_intensity * cache_bw[i]['value'])
-        cache_bound_performance[i] = np.minimum(cache_bound_performance[i], peak_flopss)
-
-        if matching_dict is not None and cache_bw[i]['value'] == matching_dict['value']:
-            cache_bound_performance[i] = cache_bound_performance[i][~np.isin(cache_bound_performance[i], peak_flopss)]
-            continue  
-        #cache_bound_performance[i] = cache_bound_performance[i][~np.isin(cache_bound_performance[i], mem_bound_performance)]
-    
-
-    # TODO: decide what to do with the following. Do we have this kind of data? It looks weird on the chart
-    # filter_x_idxs = np.where(x_data >= 0)[0]
-    # filter_y_idxs = np.where(y_data >= mem_bound_performance[0])[0]
-    # filter_idxs = np.intersect1d(filter_x_idxs, filter_y_idxs)
-    # x_data = x_data[filter_idxs]
-    # y_data = y_data[filter_idxs]
-    
-    # Create figure
-    fig = go.Figure()
-
-    dash_type = ['dot', 'dash', 'longdash', 'dashdot', 'longdashdot']
-
-    # Add data
     dots_fig = curve_utils.get_roofline_markers_dots_fig(df, x_data, y_data, markers_color, stress_score_scale, markers_transparency);
     fig.add_trace(dots_fig)
+
     
-    fig.add_trace(go.Scatter(x=operational_intensity, y=mem_bound_performance, 
-                             mode='lines', line=dict(color='black', width=2),
+    fig.add_trace(go.Scatter(x=[0, peak_flopss/peak_bw_gbs, max_x_value], y=[0, peak_flopss, peak_flopss], mode='lines', line=dict(color='black', width=2),
                              name=f'Roofline',
                              hoverlabel=dict(namelength=0),
-                             hovertemplate=f'<b>Compute Roofline</b><br>' +
+                             
+                             hovertemplate=f'<b>Roofline</b><br>' +
                                             'Operational Intensity: %{x} (FLOPS/Byte)<br>' +
-                                            'Performance: %{y} (GFLOPS/s)<br>',
-                                            
-                             ))
-    for i in range(len(cache_bound_performance)):
+                                            'Performance: %{y} (GFLOPS/s)<br>'))
+    
+    dash_type = ['dot', 'dash', 'longdash', 'dashdot', 'longdashdot']
+
+    for i in range(len(cache_bw)):
+        cache_elbow = peak_flopss/cache_bw[i]['value']
         level= cache_bw[i]['level']
-        fig.add_trace(go.Scatter(x=operational_intensity, y=cache_bound_performance[i],
-                                mode='lines', line=dict(color='black', width=2, dash=dash_type[i]),
-                                #TODO: Decidir si es vol posar el valor de cada bw o no
-                                hoverlabel=dict(namelength=0),
+        fig.add_trace(go.Scatter(x=[0, cache_elbow], y=[0, peak_flopss], mode='lines', line=dict(color='black', width=2, dash=dash_type[i]),
+            hoverlabel=dict(namelength=0),
                                 hovertemplate=f'<b>{f"Cache BW roof ({level})" if level != "DRAM" else level}</b><br>BW: {cache_bw[i]["unit"]}<br>' +
                                           'Operational Intensity: %{x} (FLOPS/Byte)<br>' +
                                           'Performance: %{y} (GFLOPS/s)<br>',
                                 name=f'{cache_bw[i]["level"]} ({cache_bw[i]["unit"]})', showlegend=True))
-            
+
     fig.update_layout(
         legend=dict(
             orientation="h",
@@ -191,29 +174,6 @@ def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_tran
         )
     )
 
-    # Annotations for Memory and Compute Roofs
-    # Memory roof values
-    #TODO: Fix this annotation to be dynamically centered on the memory roof
-    # mem_roof_values = np.unique(mem_bound_performance)
-    # # Operational intensity values for which the memory roof discurs
-    # mem_bound_x_values = operational_intensity[:len(mem_roof_values)]
-
-    # memory_angles = np.arctan(peak_bw_gbs / operational_intensity) * 180 / np.pi
-    # memory_angle = memory_angles[int(len(memory_angles) * 0.43)]
-
-
-    # fig.add_annotation(
-    #     # We later set x and y axis to log scale, so we need to set x and y in log10 scale
-    #     # Set x at 1/3 of the operational intensity values corresponding to the memory roof
-    #     x=np.log10(mem_bound_x_values[len(mem_bound_x_values) // 2])-0.1, # We subtract .1 to separate title from graph a bit
-    #     # Set y to be 65% of the memory roof values
-    #     y=np.log10(mem_roof_values[int(len(mem_roof_values) * 0.4)]),
-    #     text=f"<b>Memory BW roof ({matching_dict['level']})<br>({matching_dict['unit']})</b>",
-    #     showarrow=False,
-    #     font=dict(size=10, color='black'),
-    #     textangle=-memory_angle,
-    # )
-    
     if markers_color == 'stress_score':
         color_bar_trace = go.Scatter(x=[None], y=[None], mode='markers', 
             name='stress_score_trace',
@@ -230,21 +190,26 @@ def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_tran
 
         fig.add_trace(color_bar_trace)
 
-
-    
-    # x_pos_compute = 7 * peak_flopss / peak_bw_gbs
     fig.add_annotation(
-        # Set x at 50% (indicated as 0.5 and 'paper')
-        x=0.5,
-        xref='paper',
-        # Set y to be above the top y value (remember y is in log scale)
+        x=3*(np.log10(peak_flopss/peak_bw_gbs + max_x_value)/4),
         y=np.log10(peak_flopss * 1.25),
-        text=f"<b>Compute roof ({(0.001*peak_flopss):.1f} TFLOPS/s)</b>",
+        text=f"<b>Compute roof ({(0.000001*peak_flopss):.2f} TFLOPS/s)</b>",
         showarrow=False,
         font=dict(size=12, color='black'),
     )
 
-    # Set labels, title, and layout
+ 
+    angle = math.atan2(np.log10(peak_flopss), np.log10(peak_flopss/peak_bw_gbs)) * 180 / math.pi
+
+    fig.add_annotation(
+        x=np.log10((peak_flopss/peak_bw_gbs)/9),
+        y=np.log10(peak_flopss/9)+0.3,
+        text=f"<b>Memory BW roof<br>({peak_bw_gbs:.1f} GB/s)</b>",
+        showarrow=False,
+        textangle=-angle*0.75,
+        font=dict(size=12, color='black')
+    )
+
     fig.update_layout(
         title={
             'text': graph_title,
@@ -261,14 +226,7 @@ def plotCARM(df, peak_bw_gbs, peak_flopss, cache_bw, markers_color, markers_tran
         xaxis_title="Operational Intensity (FLOPS/Byte)",
         yaxis_title="Performance (GFLOPS/s)",
         showlegend=True,
-        # xgrid=True,
-        # ygrid=True
     )
 
-
-
     
-
-
-
     return fig
