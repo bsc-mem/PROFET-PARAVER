968f7032e87a0a133b0ca4bb89a373aed28fe5d4
vxirau <victor.xirau@salle.url.edu>
2023-10-11 17:31:28 +0200

    overview minor changes

--- src/py_profet/interactive_plots/overview.py
+++ src/py_profet/interactive_plots/overview.py
@@ -6,9 +6,10 @@ import pandas as pd
 def plot(df: pd.DataFrame = None, division: float = 1.0):
     
     fig = go.Figure(data=[go.Scatter(x=division*df['timestamp'], y=df['stress_score'], mode='lines+markers')])
-    fig.update_layout(title=f'Stress Score Over Time | Intervals of {division}s', xaxis_title='Seconds (Timestamps)', yaxis_title='MAX Stress Score each second')
+    fig.update_layout(title=f'Stress Score Over Time | Intervals of {division}s', xaxis_title='Seconds (Timestamps)', yaxis_title='Max. Stress Score')
+    fig.update_yaxes(range=[0, 1])
     fig.update_traces(hovertemplate=(
-        '<b>MAX Stress Score</b><br>' +
+        '<b>Max Stress Score</b><br>' +
         'Stress Score: %{y}<br>' +
         'Timestamp: %{x} seconds<extra></extra>'
     ))
